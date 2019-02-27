4b1ea2236c14f5257c85bf8aa99b3eef58a952eb
drunken <drunken@drunkendeMacBook-Pro.local>
2019-02-27 12:06:04 +0800

    add readme and init demo code

---
+++ taker.js
@@ -0,0 +1,13 @@
+const client = require('./client');
+
+// taker 对应的 eos 和 eth账号私钥
+const privateKey = {
+    ETH: '02DB31472EB7FF0F42C8E815BFAA541CE563AA894CB3A66E4C6616427A6BD954',
+    EOS: '5JSRAcfALhELVvTK59umFEXSzY4MkBCL3SPajSZw1BqHyoLtH79'
+};
+const kofoId = '023fbf8ded0155ef73b25c19e20a4a92f984ffe621e8d1b0a2572e593469c1016e';
+const clean = true;
+
+client.run(privateKey, kofoId, clean, 'taker');
+
+
